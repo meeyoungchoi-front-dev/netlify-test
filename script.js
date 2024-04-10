1b49f99e444da381658bd7d274ef98e6852d49c6
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-10 12:58:13 +0900

    button addEventListener 함수 변경
    button addEventListener 함수 내부 buttonDiv innerText 변경

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ const buttonThree = document.querySelector('#button3');
 const buttonDiv = document.querySelector('.buttonDiv');
 
 buttonOne.addEventListener('click', function(e) {
-  alert('회원가입 버튼 클릭');
-  buttonDiv.textContent = '회원가입 영역';
+  buttonDiv.textContent = '버튼1 영역';
   buttonDiv.style.backgroundColor = "red";
 })
 
 buttonTwo.addEventListener('click', function(e) {
-  alert('로그인 버튼 클릭');
-  buttonDiv.textContent = '로그인 영역';
+  buttonDiv.textContent = '버튼2 영역';
   buttonDiv.style.backgroundColor = "purple";
 })
 
 buttonThree.addEventListener('click', function(e) {
-  alert('비밀번호 찾기 버튼 클릭');
-  buttonDiv.textContent = '비밀번호 찾기 영역';
+  buttonDiv.textContent = '버튼3 영역';
   buttonDiv.style.backgroundColor = "green";
 })
